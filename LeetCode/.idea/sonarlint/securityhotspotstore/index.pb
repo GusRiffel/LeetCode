284ff4a12f1c53f63c6c91ee1107eee53ae9e568
GusRiffel <gustavoriffell@gmail.com>
2023-06-21 12:01:21 +0100

    Starts MaxDepth

--- LeetCode/.idea/sonarlint/securityhotspotstore/index.pb
+++ LeetCode/.idea/sonarlint/securityhotspotstore/index.pb
@@ -1,5 +1,3 @@
 
 h
 8src/com/GusRiffel/string/RemoveOutermostParentheses.java,e/b/eb4232d88eb86517efd3665a098bfcfc788530e3
-k
-;src/com/GusRiffel/string/DifferentWaysToAddParentheses.java,7/6/76e2e1dbc1ee99e7e7a586643c42f39ef1aba5b2
---
+++ LeetCode/src/com/GusRiffel/string/MaxDepth.java
@@ -0,0 +1,11 @@
+package com.GusRiffel.string;
+
+public class MaxDepth {
+    public static void main(String[] args) {
+        System.out.println(maximumDepth("(1+(2*3)+((8)/4))+1"));
+    }
+
+    public static int maximumDepth(String s) {
+        return 0;
+    }
+}
