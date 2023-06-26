75726e66d652ae2d75f13be7b617a05488705212
GusRiffel <gustavoriffell@gmail.com>
2023-06-26 14:08:14 +0100

    Completes Valid Anagram

--- LeetCode/.idea/sonarlint/issuestore/index.pb
+++ LeetCode/.idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,13 @@ a
 1src/com/GusRiffel/arrays/runningSumOf1dArray.java,2/8/28bcc40117eedbe52e94decae7f770f6786138b4
 {
 Ksrc/com/GusRiffel/string/substringcounting/NumberofWonderfulSubstrings.java,a/d/ad2f3903c69a7b4bbbcbb765c06e7d80a3a44490
+q
+Asrc/com/GusRiffel/string/parenthesis/LongestValidParentheses.java,6/b/6b0c21e142aa0c6b283d005cc36ff6dd44292ea7
+w
+Gsrc/com/GusRiffel/string/parenthesis/DifferentWaysToAddParentheses.java,2/4/24c966b31fd743a0bc6b55dd55c81494b7772a38
+m
+=src/com/GusRiffel/string/parenthesis/GenerateParentheses.java,7/1/71cf725707891113d50b35c02fd8c498f53f4179
+e
+5src/com/GusRiffel/string/types/IsomorphicStrings.java,4/2/4243702ce3c120c9fb149403cafd472a19df9ddf
+`
+0src/com/GusRiffel/string/types/ValidAnagram.java,5/b/5b1ef2d0dd32f40c9d6a3cb781e4757a130ed9b6
--- LeetCode/src/com/GusRiffel/string/types/ValidAnagram.java
+++ LeetCode/src/com/GusRiffel/string/types/ValidAnagram.java
@@ -1,2 +1,41 @@
-package com.GusRiffel.string.types;public class ValidAnagram {
+package com.GusRiffel.string.types;
+//Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+//
+//        An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+//
+//
+//
+//        Example 1:
+//
+//        Input: s = "anagram", t = "nagaram"
+//        Output: true
+//        Example 2:
+//
+//        Input: s = "rat", t = "car"
+//        Output: false
+//
+//
+//        Constraints:
+//
+//        1 <= s.length, t.length <= 5 * 104
+//        s and t consist of lowercase English letters.
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("rac", "car"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        int map1[] = new int[127];
+
+        for (int i = 0; i < s.length(); i++) {
+            map1[s.charAt(i)]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            map1[t.charAt(i)]--;
+        }
+        return Arrays.stream(map1).distinct().count() == 1;
+    }
 }
